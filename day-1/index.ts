9137fdef9ceb8991c86e7f9296e857cd7421d6db
Rens Boeser <r.boeser@sencon.nl>
2024-12-01 21:15:29 +0100

    Fix variable naming

--- day-1/index.ts
+++ day-1/index.ts
@@ -2,43 +2,43 @@ const parseInput = (): Array<Array<number>> => {
   const inputText = Deno.readTextFileSync("day-1/input.txt")
   const pairs = inputText.split("\n")
 
-  const array1: Array<number> = []
-  const array2: Array<number> = []
+  const numbers1: Array<number> = []
+  const numbers2: Array<number> = []
   for (const pair of pairs) {
     const [num1, num2] = pair.split("   ").map(Number)
 
-    array1.push(num1)
-    array2.push(num2)
+    numbers1.push(num1)
+    numbers2.push(num2)
   }
 
-  return [array1, array2]
+  return [numbers1, numbers2]
 }
 
-const part1 = (array1: Array<number>, array2: Array<number>) => {
-  array1.sort((a, b) => a - b)
-  array2.sort((a, b) => a - b)
+const part1 = (numbers1: Array<number>, numbers2: Array<number>): number => {
+  const sortedNumbers1 = numbers1.toSorted((a, b) => a - b)
+  const sortedNumbers2 = numbers2.toSorted((a, b) => a - b)
 
-  const distanceArray = array1.map((num, index) =>
-    Math.abs(num - array2[index])
+  const numberDisplacement = sortedNumbers1.map((num, index) =>
+    Math.abs(num - sortedNumbers2[index])
   )
 
-  return distanceArray.reduce((acc, curr) => acc + curr, 0)
+  return numberDisplacement.reduce((acc, curr) => acc + curr, 0)
 }
 
-const part2 = (array1: Array<number>, array2: Array<number>) => {
-  const similarityScoreArray = array1.map((num) => {
-    const elementFrequency = array2.filter((num2) => num2 === num).length
-    return num * elementFrequency
+const part2 = (numbers1: Array<number>, numbers2: Array<number>): number => {
+  const similarityScores = numbers1.map((num) => {
+    const occurrenceCount = numbers2.filter((num2) => num2 === num).length
+    return num * occurrenceCount
   })
 
-  return similarityScoreArray.reduce((acc, curr) => acc + curr, 0)
+  return similarityScores.reduce((acc, curr) => acc + curr, 0)
 }
 
 if (import.meta.main) {
   console.log("Day 1: Historian Hysteria")
 
-  const [array1, array2] = parseInput()
+  const [numbers1, numbers2] = parseInput()
 
-  console.log("Result of part 1:", part1(array1, array2))
-  console.log("Result of part 2:", part2(array1, array2))
+  console.log("Result of part 1:", part1(numbers1, numbers2))
+  console.log("Result of part 2:", part2(numbers1, numbers2))
 }
